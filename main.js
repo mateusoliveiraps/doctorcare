07c3347f10eb4229e057a084ad04b6671d01a096
Mateus dos Santos <32557031+mateusoliveiraps@users.noreply.github.com>
2022-05-18 08:31:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+//Adiciona Evento de Scroll na Window.
+window.addEventListener('scroll', onScroll)
+
+//roda a função uma unica vez antes.
+onScroll()
+
+function onScroll() {
+  showNavOnScrol()
+  showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  //linha alvo
+  const targetLine = scrollY + innerHeight / 2
+  
+  //verificar se a secao passou da linha
+
+  //o topo da seção
+  const sectionTop = section.offsetTop
+  //a altura da seção
+  const sectionHeight = section.offsetHeight
+  // o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+  //verificar se a base está abaixo da linha alvo
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  //remove o active antes de entrar no laço de repetição
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScrol() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+#home, 
+#home img, 
+#home .stats, 
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about .content`)
